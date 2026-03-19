e79b1781edc5ec4a8caf3aeb8ef8f3df5550d6c1
VictorZavalaa <oxvo210461@upemor.edu.mx>
2026-03-19 13:57:41 -0600

    subida cambios y modelos y  migraciones

--- app/Models/Vuelo.php
+++ app/Models/Vuelo.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vuelo extends Model
 {
-    //
+    protected $fillable = ['horaSalidaTerminal', 'horaEstimadaLlegada', 'terSalida', 'terLlegada', 'ciudadSalida', 'ciudadLlegada', 'nomPiloto'];
 }
